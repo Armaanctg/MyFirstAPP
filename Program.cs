7215d7c2a295a7f2e258bb5930ad4bb8134aa239
Armaanctg <armaan.wub@gmail.com>
2020-03-10 20:11:05 +0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Jagged_Array
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            String[][] JaggedArray =new string [3][]
+           {
+                new string[] {"BBA","EMBA"},
+                new string[] { "Bsc", "MSc", "SSc" },
+                new string[] { "Ssc" }
+            };
+
+            Console.WriteLine(JaggedArray[0] [0]);
+            Console.WriteLine(JaggedArray[0] [1]);
+
+            Console.WriteLine(JaggedArray[1] [0]);
+            Console.WriteLine(JaggedArray[1] [1]);
+
+            Console.WriteLine(JaggedArray[1][2]);
+
+            Console.WriteLine(JaggedArray[2][0]);
+        }
+           
+
+    }
+}
